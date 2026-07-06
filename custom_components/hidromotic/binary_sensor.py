83b57a91adba3f0bf295809d8210e508d72e2d53
Damian Alejandro Fernandez Sosa <damian@fernandezsosa.com.ar>
2026-07-06 14:59:43 -0300

    Sensor fixes

--- custom_components/hidromotic/binary_sensor.py
+++ custom_components/hidromotic/binary_sensor.py
@@ -122,8 +122,8 @@ class HidromoticTankFullSensor(
         if tank:
             nivel = tank.get("nivel", 0xFF)
             level_map = {
-                0: "full",
-                1: "empty",
+                0: "empty",
+                1: "full",
                 2: "sensor_fail",
                 3: "level_fail",
                 4: "medium",
@@ -200,8 +200,8 @@ class HidromoticTankEmptySensor(
         if tank:
             nivel = tank.get("nivel", 0xFF)
             level_map = {
-                0: "full",
-                1: "empty",
+                0: "empty",
+                1: "full",
                 2: "sensor_fail",
                 3: "level_fail",
                 4: "medium",
